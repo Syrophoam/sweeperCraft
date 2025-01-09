d7b05df07cfd0b5f8583019085044daaf7ab2415
Syrophoam <jamesfullerton2006@gmail.com>
2025-01-09 21:25:59 +1300

    Initial Commit

---
+++ sweeperCraft/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sweeperCraft
+//
+//  Created by syro Fullerton on 09/01/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
